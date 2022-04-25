3be56bc63f5dbc54c7e9337f1660595f8ef5c02f
Chunghyun <donerd@handong.ac.kr>
2022-04-25 21:34:30 +0900

    implement load Data

--- manager.c
+++ manager.c
@@ -62,7 +62,25 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	if((fp = fopen("product.txt", "r")) == NULL)
+	{
+		return count;
+	}
 
+	else
+	{
+		while(1)
+		{
+			if(fscanf(fp, "%[^,], %d, %d\n", p[count].name, &p[count].weight, &p[count].price) != -1)
+			{
+				count ++;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
 
 
 
